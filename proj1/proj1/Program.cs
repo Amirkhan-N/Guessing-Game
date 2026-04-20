67aa97a5a5368741c206e85276bd7cdc68d8f776
K-one <nurbaiamirhan@gmail.com>
2026-04-20 14:44:04 +0500

    Add files via upload

---
+++ proj1/proj1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.Design;
+
+namespace proj1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+            int secret = rng.Next(1, 101);
+            int attemps = 0;
+
+            Console.WriteLine("Guess a number between 1 and 100!!!1!1!!");
+
+            while (true)
+            {
+                Console.Write("Your guess: ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out int guess))
+                {
+                    Console.WriteLine("only numbers");
+                    continue;
+                }
+                attemps++;
+
+                if (guess < secret)
+                {
+                    Console.WriteLine("higher!");
+                }
+                else if (guess > secret)
+                {
+                    Console.WriteLine("lower!");
+                }
+                else
+                {
+                    Console.WriteLine($"Correct!! it took you {attemps} attempts!!");
+                    break;
+                }
+            }
+        }
+    }
+}
